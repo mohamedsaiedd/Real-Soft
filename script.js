d40bea19a6459016a7445ee5bc01bfb301149497
mohamedsaiedd <mohamed.saied.fathallah@gmail.com>
2022-09-01 13:25:52 +0200

    home L : tetsimonials final

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $('.autoplay').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 100000,
-    prevArrow:"<button type='button' class='slick-prev btn  btn-default pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
-    nextArrow:"<button type='button' class='slick-next btn btn-defaultpull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+    autoplaySpeed: 2000,
+    nextArrow:"<button type='button' class='slick-next btn btn-default pull-right'><i class='fa fa-arrow-right' aria-hidden='true'></i></button>",
+    prevArrow:"<button type='button' class='slick-prev btn  btn-default pull-left'><i class='fa fa-arrow-left' aria-hidden='true'></i></button>",
   });
