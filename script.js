1740f4ef710ab6f1a47a477bc2200ee70839e238
mohamedsaiedd <mohamed.saied.fathallah@gmail.com>
2022-09-01 12:47:06 +0200

    home L : testimonials done

---
+++ script.js
@@ -0,0 +1,8 @@
+$('.autoplay').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 100000,
+    prevArrow:"<button type='button' class='slick-prev btn  btn-default pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:"<button type='button' class='slick-next btn btn-defaultpull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+  });
