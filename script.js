04152c67a95f90a2b4093e9907e359a168b72f31
mohamedsaiedd <moahmed.saied.fathallah@gmail.com>
2022-09-03 20:27:37 +0200

    responsive edit's final : done

--- script.js
+++ script.js
@@ -6,13 +6,19 @@ $('.autoplay').slick({
     autoplay: true,
     autoplaySpeed: 2000,
     responsive: [
-        {
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 2,
+        }
+      },{
           breakpoint: 600,
           settings: {
             slidesToShow: 1,
           }
         },
     ],
+  
     nextArrow:"<button type='button' class='slick-next btn btn-default pull-right'><i class='fa fa-arrow-right' aria-hidden='true'></i></button>",
     prevArrow:"<button type='button' class='slick-prev btn  btn-default pull-left'><i class='fa fa-arrow-left' aria-hidden='true'></i></button>",
   });
