4bf09165dcc9938ca535b26aa235f738c8f1cda9
mohamedsaiedd <mohamed.saied.fathallah@gmail.com>
2022-09-03 11:59:43 +0200

    Responsive handel mobile screen : done

--- build/script.js
+++ build/script.js
@@ -5,6 +5,14 @@ $('.autoplay').slick({
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
+    responsive: [
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+          }
+        },
+    ],
     nextArrow:"<button type='button' class='slick-next btn btn-default pull-right'><i class='fa fa-arrow-right' aria-hidden='true'></i></button>",
     prevArrow:"<button type='button' class='slick-prev btn  btn-default pull-left'><i class='fa fa-arrow-left' aria-hidden='true'></i></button>",
   });
